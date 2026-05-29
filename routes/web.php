f1b1e61550c40f2e2517c2dcbc10b28a58b879e6
Nguyen An Phu <anphu12t2@gmail.com>
2026-05-29 14:37:27 +0700

    Revert "init template"
    This reverts commit a4f7cef87a9072917d5cadf22aed0d802eb19982.

--- routes/web.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-// DOPFood SPA – All web requests serve the same blade view.
-// Client-side hash router handles navigation.
-Route::get('/{any}', function () {
-    return view('welcome');
-})->where('any', '.*');
